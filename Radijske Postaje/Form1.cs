10ca6adcb467fcfabf68d0608b5e3339cb7f9b3d
Ghost Rider <dejan.ds123@gmail.com>
2020-12-23 11:52:33 +0100

    NpgSQL nuget package + testiranje

--- Radijske Postaje/Form1.cs
+++ Radijske Postaje/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace Radijske_Postaje
 {
@@ -16,5 +17,27 @@ namespace Radijske_Postaje
         {
             InitializeComponent();
         }
+
+        /*Testiranje
+        void izpisi()
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=dumbo.db.elephantsql.com; User Id=ejdvbvlw;" + "Password=oLgUkOCXPTKG_2bvDFB1NnSPgp3tcDxj; Database=ejdvbvlw;"))
+            {
+                
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM zvrsti", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    string ime = reader.GetString(1);
+                    string opis = reader.GetString(2);
+
+                    MessageBox.Show(ime + " " + opis);
+                }
+                
+            }
+        }*/
+
+
     }
 }
