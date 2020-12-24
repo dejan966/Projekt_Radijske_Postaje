ace71251191da500562b9b72aef636c57a67324f
Ghost Rider <dejan.ds123@gmail.com>
2020-12-24 16:57:29 +0100

    Prvi tab narejen (Radijske Postaje)

--- Radijske Postaje/Form1.Designer.cs
+++ Radijske Postaje/Form1.Designer.cs
@@ -29,13 +29,301 @@ namespace Radijske_Postaje
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Btn_Delete = new System.Windows.Forms.Button();
+            this.Btn_Update = new System.Windows.Forms.Button();
+            this.Btn_Insert = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(793, 515);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.Btn_Delete);
+            this.tabPage1.Controls.Add(this.Btn_Update);
+            this.tabPage1.Controls.Add(this.Btn_Insert);
+            this.tabPage1.Controls.Add(this.textBox8);
+            this.tabPage1.Controls.Add(this.textBox7);
+            this.tabPage1.Controls.Add(this.textBox6);
+            this.tabPage1.Controls.Add(this.textBox5);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(785, 486);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Radijske Postaje";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Delete
+            // 
+            this.Btn_Delete.Location = new System.Drawing.Point(110, 449);
+            this.Btn_Delete.Name = "Btn_Delete";
+            this.Btn_Delete.Size = new System.Drawing.Size(75, 31);
+            this.Btn_Delete.TabIndex = 13;
+            this.Btn_Delete.Text = "Zbriši";
+            this.Btn_Delete.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Update
+            // 
+            this.Btn_Update.Location = new System.Drawing.Point(171, 412);
+            this.Btn_Update.Name = "Btn_Update";
+            this.Btn_Update.Size = new System.Drawing.Size(75, 31);
+            this.Btn_Update.TabIndex = 12;
+            this.Btn_Update.Text = "Posodobi";
+            this.Btn_Update.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Insert
+            // 
+            this.Btn_Insert.Location = new System.Drawing.Point(44, 412);
+            this.Btn_Insert.Name = "Btn_Insert";
+            this.Btn_Insert.Size = new System.Drawing.Size(75, 31);
+            this.Btn_Insert.TabIndex = 11;
+            this.Btn_Insert.Text = "Vnesi";
+            this.Btn_Insert.UseVisualStyleBackColor = true;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(44, 384);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(202, 22);
+            this.textBox8.TabIndex = 10;
+            this.textBox8.Text = "Regija";
+            this.textBox8.Click += new System.EventHandler(this.textBox8_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(44, 349);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(202, 22);
+            this.textBox7.TabIndex = 9;
+            this.textBox7.Text = "Zvrst";
+            this.textBox7.Click += new System.EventHandler(this.textBox7_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(44, 312);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(202, 22);
+            this.textBox6.TabIndex = 8;
+            this.textBox6.Text = "Twitter";
+            this.textBox6.Click += new System.EventHandler(this.textBox6_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(44, 275);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(202, 22);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.Text = "Youtube";
+            this.textBox5.Click += new System.EventHandler(this.textBox5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(44, 237);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(202, 22);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "Spletna stran";
+            this.textBox4.Click += new System.EventHandler(this.textBox4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(44, 199);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(202, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "Jezik";
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(44, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(202, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Frekvenca";
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(44, 93);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(241, 55);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "Opis";
+            this.richTextBox1.Click += new System.EventHandler(this.richTextBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(41, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Radijske Postaje";
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.textBox1.Location = new System.Drawing.Point(44, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(202, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Naslov";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(446, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(311, 180);
+            this.listBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(785, 486);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Lokalni studiji";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(18, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 25);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(18, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 25);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(18, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 25);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(18, 349);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 25);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(18, 384);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 25);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(793, 515);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Urejanje radijskih postaj";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button Btn_Delete;
+        private System.Windows.Forms.Button Btn_Update;
+        private System.Windows.Forms.Button Btn_Insert;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Radijske Postaje/Form1.cs
+++ Radijske Postaje/Form1.cs
@@ -13,9 +13,135 @@ namespace Radijske_Postaje
 {
     public partial class Form1 : Form
     {
+        string a = "Naslov";
+        string b = "Opis";
+        string c = "Frekvenca";
+        string d = "Jezik";
+        string j = "Spletna stran";
+        string f = "Youtube";
+        string g = "Twitter";
+        string h = "Zvrst";
+        string i = "Regija";
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {           
+            textBox1.Clear();
+            richTextBox1.Text = b;
+            textBox2.Text = c;
+            textBox3.Text = d;
+            textBox4.Text = j;
+            textBox5.Text = f;
+            textBox6.Text = g;
+            textBox7.Text = h;
+            textBox8.Text = i;
+        }
+
+        private void richTextBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = a;
+            richTextBox1.Clear();
+            textBox2.Text = c;
+            textBox3.Text = d;
+            textBox4.Text = j;
+            textBox5.Text = f;
+            textBox6.Text = g;
+            textBox7.Text = h;
+            textBox8.Text = i;
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = a;
+            richTextBox1.Text = b;
+            textBox2.Clear();
+            textBox3.Text = d;
+            textBox4.Text = j;
+            textBox5.Text = f;
+            textBox6.Text = g;
+            textBox7.Text = h;
+            textBox8.Text = i;
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = a;
+            richTextBox1.Text = b;
+            textBox2.Text = c;
+            textBox3.Clear();
+            textBox4.Text = j;
+            textBox5.Text = f;
+            textBox6.Text = g;
+            textBox7.Text = h;
+            textBox8.Text = i;
+        }
+
+        private void textBox4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = a;
+            richTextBox1.Text = b;
+            textBox2.Text = c;
+            textBox3.Text = d;
+            textBox4.Clear();
+            textBox5.Text = f;
+            textBox6.Text = g;
+            textBox7.Text = h;
+            textBox8.Text = i;
+        }
+
+        private void textBox5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = a;
+            richTextBox1.Text = b;
+            textBox2.Text = c;
+            textBox3.Text = d;
+            textBox4.Text = j;
+            textBox5.Clear();
+            textBox6.Text = g;
+            textBox7.Text = h;
+            textBox8.Text = i;
+        }
+
+        private void textBox6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = a;
+            richTextBox1.Text = b;
+            textBox2.Text = c;
+            textBox3.Text = d;
+            textBox4.Text = j;
+            textBox5.Text = f;
+            textBox6.Clear();
+            textBox7.Text = h;
+            textBox8.Text = i;
+        }
+
+        private void textBox7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = a;
+            richTextBox1.Text = b;
+            textBox2.Text = c;
+            textBox3.Text = d;
+            textBox4.Text = j;
+            textBox5.Text = f;
+            textBox6.Text = g;
+            textBox7.Clear();
+            textBox8.Text = i;
+        }
+
+        private void textBox8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = a;
+            richTextBox1.Text = b;
+            textBox2.Text = c;
+            textBox3.Text = d;
+            textBox4.Text = j;
+            textBox5.Text = f;
+            textBox6.Text = g;
+            textBox7.Text = h;
+            textBox8.Clear();
         }
 
         /*Testiranje
